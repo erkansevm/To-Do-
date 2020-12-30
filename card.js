97bf9fb7f6f531dbdbb4a8fc45039fb1ae800577
erkansevm <erkansevm528@gmail.com>
2020-12-30 11:20:07 +0300

    card-task handler

--- card.js
+++ card.js
@@ -1,8 +1,15 @@
 
-function Card(employee,id) {
-  this.employee = employee
+function Card(id) {
   this.id = id;
   this.taskList = [];
   this.container;
+}
+
+Card.prototype.isEmpty = function (){
+  if (this.taskList.length == 0) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
